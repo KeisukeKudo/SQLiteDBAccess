c9232cb4ecea5382ec55e5acba06e290372bcf27
keisuke kudo <melchior.balthasar.casper326@gmail.com>
2017-11-18 13:21:42 +0900

    Create DBAccess.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqliteDBAccess;
+
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+            using (var dba = new DBAccess("TestRecord")) {
+                CreateTable(dba);
+                Console.WriteLine(InsertRecord(dba));
+
+                foreach (var e in Query(dba)) {
+                    Console.WriteLine($"ID: {e.Id} NAME: {e.Name} AGE: {e.Age}");
+                }
+
+                Console.WriteLine(UpdateRecod(dba));
+
+                foreach (var e in Query(dba)) {
+                    Console.WriteLine($"ID: {e.Id} NAME: {e.Name} AGE: {e.Age}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// TESTテーブルを作成
+        /// </summary>
+        /// <param name="dba"></param>
+        private static void CreateTable(DBAccess dba) {
+            var sql = @"
+CREATE TABLE IF NOT EXISTS employee (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT
+   ,Name TEXT
+   ,Age INTEGER
+)";
+            dba.BeginTransaction();
+            dba.Execute(sql);
+            dba.Commit();
+        }
+
+
+        /// <summary>
+        /// レコードを追加
+        /// </summary>
+        /// <param name="dba"></param>
+        /// <returns></returns>
+        private static int InsertRecord(DBAccess dba) {
+            dba.SetParameters("Name", "Kudo");
+            dba.SetParameters("Age", 30);
+            var sql  = @"
+INSERT INTO employee
+(Name, Age)
+VALUES
+(@Name, @Age)";
+
+            dba.BeginTransaction();
+            var result = dba.Execute(sql);
+            dba.Commit();
+            return result;
+        }
+
+
+        /// <summary>
+        /// レコードを更新
+        /// </summary>
+        /// <param name="dba"></param>
+        /// <returns></returns>
+        private static int UpdateRecod(DBAccess dba) {
+            dba.SetParameters("Name", "Kudo");
+            dba.SetParameters("Age", 31); // 30から31にアップロード
+            var sql = @"
+UPDATE employee
+SET Age = @Age
+WHERE Name = @Name";
+
+            dba.BeginTransaction();
+            var result = dba.Execute(sql);
+            dba.Commit();
+            return result;
+        }
+
+
+        /// <summary>
+        /// 全データを取得
+        /// </summary>
+        /// <param name="dba"></param>
+        /// <returns></returns>
+        private static IEnumerable<TestEntity> Query(DBAccess dba) {
+            var sql = @"
+SELECT * FROM employee";
+            return dba.Query<TestEntity>(sql);
+        }
+
+
+
+        /// <summary>
+        /// TEST用Entityクラス
+        /// </summary>
+        class TestEntity {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ SqliteDBAccess/DBAccess.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using Dapper;
+
+
+namespace SqliteDBAccess {
+    public class DBAccess : IDisposable {
+
+        private readonly SQLiteConnection ConectinObject;
+
+        private SQLiteTransaction tran;
+
+        private DynamicParameters Parameters = new DynamicParameters();
+
+
+        /// <summary>
+        /// パラメーター設定
+        /// </summary>
+        /// <param name="parameterName">プレースホルダ名</param>
+        /// <param name="parameter">値</param>
+        public void SetParameters(string parameterName, object parameter = null) {
+            this.Parameters.Add(parameterName, parameter);
+        }
+        
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="connectionName">configファイルの接続文字列セクションのnameを設定</param>
+        public DBAccess(string connectionName) {
+            //参照元のconfigファイルから接続文字列を取得
+            var conectionString = new SQLiteConnectionStringBuilder() {
+                DataSource = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString
+            };
+
+            ConectinObject = new SQLiteConnection(conectionString.ToString());
+            ConectinObject.Open();
+        }
+
+
+        /// <summary>
+        /// SELECTを実行
+        /// </summary>
+        /// <typeparam name="T">取得したい型</typeparam>
+        /// <param name="sql">SELECT文</param>
+        /// <returns></returns>
+        public IEnumerable<T> Query<T>(string sql) {
+            return ConectinObject.Query<T>(sql, Parameters);
+        }
+
+
+        /// <summary>
+        /// DML文を実行
+        /// </summary>
+        /// <param name="sql">DML文</param>
+        /// <returns>実行時影響があったレコード数</returns>
+        public int Execute(string sql) {
+            return ConectinObject.Execute(sql, Parameters);
+        }
+
+
+        /// <summary>
+        /// Transaction
+        /// </summary>
+        public void BeginTransaction() {
+            this.tran = ConectinObject.BeginTransaction();
+        }
+
+
+        /// <summary>
+        /// Commit
+        /// </summary>
+        public void Commit() {
+            try {
+                this.tran?.Commit();
+            } catch {
+                this.RollBack();
+            }
+        }
+
+
+        /// <summary>
+        /// Rollback
+        /// </summary>
+        public void RollBack() {
+            this.tran?.Rollback();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                }
+
+                this.tran?.Dispose();
+                this.ConectinObject?.Close();
+                this.ConectinObject?.Dispose();
+
+                disposedValue = true;
+            }
+        }
+
+        ~DBAccess() {
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            Dispose(false);
+        }
+
+        // このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+        void IDisposable.Dispose() {
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
